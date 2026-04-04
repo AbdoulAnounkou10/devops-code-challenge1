96c0a198397739406cbc958b81411f0b9f2ed92f
admin <admin@admins-MacBook-Air.local>
2026-04-04 09:19:09 -0400

    Fix frontend backend URL to use ALB endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/'
+export const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://devops-challenge-alb-1711097062.us-east-1.elb.amazonaws.com'
 export default API_URL
